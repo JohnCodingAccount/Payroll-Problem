ba94cfc77fff71aba3d1d96ad91169c98743a789
John <hcps-steelejc@henricostudents.org>
2026-02-18 10:30:26 -0500

    unfinished update

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import time
 import math
 import csv
 
-
-class Company: 
-    def __init__(self):
+class Employee: 
+    def __init__(self, name, hours, reg_hours, hol_hours, over_hours, rate, roth, fourOhOneK):
         pass
     def get_SS_tax(self):
         tax = self.gross * 0.0765 
@@ -14,25 +13,12 @@ class Company:
         tax = self.gross * 0.03
         return tax 
     def get_fed_tax(self):
-        fed = 0 
-        with open('federal.csv', 'r', newline='') as f:
-            reader = csv.reader(f)
-            # Convert all rows to a list
-            data = list(reader) 
-            for i in range(7):
-                min = int(data[i+1][0])
-                if self.gross > min:
-                    hib = float(data[i+1][2])
-                    tax = self.gross * hib 
-                    fed = fed + tax 
-                else: 
-                    break
-        return fed 
+        pass
     def get_gross(self):
         reg = self.reg_hours * self.rate
         hol = self.hol_hours * self.rate 
         over = self.over_hours * self.rate * 1.5 
-        fourohonek = self.four0oneK
+        fourohonek = self.fourOhOneK
         self.gross = reg + hol + over - fourohonek
         return self.gross 
     def get_net(self):
@@ -49,4 +35,4 @@ class Company:
         self.reg_hours = regHrs
         self.hol_hours = holHrs
         self.over_hours = oveHrs 
-#Notes: I assume 401K and Roth will be in getting the employee data, along with their percentages / set amounts, let me know if you want to change this 
+employ1 = Employee(inp1, 1,1,1,1,1,1,1,1)
