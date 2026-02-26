547bf5c9709dfb591b1ee6d0e9a935a16cfb8912
John <hcps-steelejc@henricostudents.org>
2026-02-25 20:49:38 -0500

    DONE!!!

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 from enum import Enum
 import pygame
+import time
 
 
 #Validation functions for each employee data field
@@ -120,14 +121,14 @@ class Employee:
         ss_tax = gross * 0.0765
         state_tax = gross * 0.03
         fed_tax = self.get_fed_tax()
-        roth_deduction = gross * (self.employee_data['ROTH'] / 100)
+        roth_deduction = round(gross * (self.employee_data['ROTH'] / 100),2)
         self.net = round(gross - ss_tax - state_tax - fed_tax - roth_deduction, 2)
         dub = round(gross - ss_tax - state_tax - fed_tax - roth_deduction, 2)
         return (self.net, dub, ss_tax, state_tax, fed_tax, roth_deduction, _401K_deduct)
 
     #Prints employee's most critcal data, including: name, gross income, and net income
     def __str__(self):
-        return f"Employee: {self.employee_data['NAME']} | Gross: ${self.gross:.2f} | Net: ${self.net:.2f}"
+        return f"Employee: {self.employee_data['NAME']} | Gross: ${self.gross:.2f} | Net: ${self.net:.2f} | Social Security Deduction: {self.calculate_pay()[2]} | State Deduction: {self.calculate_pay()[3]} | Federal Deduction: {self.calculate_pay()[4]} | Roth Deduction: {self.calculate_pay()[5]} | 401K Deduction: {self.calculate_pay()[6]}"
 
 class Company:
     #Defines want to do when class is initalized
@@ -201,11 +202,6 @@ class Company:
             img1 = pygame.image.load('sizedLeft.png').convert_alpha()
             img_rect1 = img1.get_rect()
             img_rect1.center = (50, height - 45)
-            load = pygame.Rect(302.5, height - 75, 50, 50)
-            flip = pygame.image.load('duct.png').convert_alpha()
-            flipped = pygame.transform.scale(flip, (50, 50)) 
-            flipped_rect = flipped.get_rect()
-            flipped_rect.center = (302.5, height - 55)
             use_images = True
         except:
             use_images = False
@@ -263,40 +259,7 @@ class Company:
         pygame.mixer.music.set_volume(0.0)
         pygame.mixer.music.play(-1)
 
-        lines = get_employee_lines(EMPLOYEE_NUM)
-
-        cards_left_1 = [
-                ("Name",          lines['name'],  white, gold),
-                ("Hours / Week",  lines['hours'], white, white),
-                ("Regular Hours", lines['reg'],   white, white),
-                ("Holiday Hours", lines['hol'],   white, white),
-                ("Overtime Hours",lines['over'],  white, white),
-            ]
-
-        card_left_2 = [
-                ("Name",          lines['name'],  white, gold),
-                ("Regular Hours Pay",  lines['reg_hours_pay'], white, white),
-                ("Holiday Hours Pay", lines['hol_hours_pay'],   white, white),
-                ("Over Hours Pay", lines['over_hours_pay'],   white, white),
-                ("401K Deduction",lines['_401k_deduct'],  white, white),
-            ]
-        cards_right_1 = [
-                ("Hourly Rate",   lines['rate'],  white, gold),
-                ("401K Rate",     lines['401k'],  white, white),
-                ("ROTH Rate",     lines['roth'],  white, white),
-                ("Gross Pay",     lines['gross'], white, gold),
-                ("Net Pay",       lines['net'],   white, green),
-            ]
-        cards_right_2 = [
-                ("Roth Deduction",   lines['roth_deduct'],  white, white),
-                ("SS/Insurance Deduction",     lines['ss_tax_deduct'],  white, white),
-                ("State Deduction",     lines['state_tax_deduct'],  white, white),
-                ("Federal Deduction",     lines['fed_tax_deduct'], white, white),
-                ("Net ",  lines['net'],   white, green),
-            ]
-
-        cards_left = cards_left_1
-        cards_right = cards_right_1
+        debounce = False
         while running:
             
             
@@ -314,67 +277,46 @@ class Company:
                         #Checking if you clicked the right arrow
                         if arrow_right_rect.collidepoint(event.pos) or (use_images and img_rect.collidepoint(event.pos)):
                             if not sliding:
-                                EMPLOYEE_NUM = (EMPLOYEE_NUM + 1) % count
-                                slide_offset = width
-                                slide_direction = -1
-                                sliding = True
-                                cards_right = cards_right_1
-                                cards_left = cards_left_1
+                                if debounce == False:
+                                    debounce = True
+                                    EMPLOYEE_NUM = (EMPLOYEE_NUM + 1) % count
+                                    slide_offset = width
+                                    slide_direction = -1
+                                    sliding = True
+                                    debounce = False
                         #Checking if you clicked the left arrow
                         elif arrow_left_rect.collidepoint(event.pos) or (use_images and img_rect1.collidepoint(event.pos)):
                             if not sliding:
-                                EMPLOYEE_NUM = (EMPLOYEE_NUM - 1) % count
-                                slide_offset = -width
-                                slide_direction = 1
-                                sliding = True
-                                cards_left = cards_left_1
-                                cards_right = cards_right_1
-                        if load.collidepoint(event.pos):
-                            if cards_left == card_left_2 and cards_right == cards_right_2:
-                                cards_left = cards_left_1
-                                cards_right = cards_right_1
-                            else:
-                                cards_left = card_left_2
-                                cards_right = cards_right_2
+                                if debounce == False:
+                                    debounce = True
+                                    EMPLOYEE_NUM = (EMPLOYEE_NUM - 1) % count
+                                    slide_offset = -width
+                                    slide_direction = 1
+                                    sliding = True
+                                    debounce = False
                 elif event.type == pygame.KEYDOWN:
                     #Checking if you pressed the left key
                     if event.key == pygame.K_LEFT:
                         if not sliding:
-                            EMPLOYEE_NUM = (EMPLOYEE_NUM - 1) % count
-                            slide_offset = -width
-                            slide_direction = 1
-                            sliding = True
-                            lines = get_employee_lines(EMPLOYEE_NUM)
+                            if debounce == False:
+                                debounce = True
+                                EMPLOYEE_NUM = (EMPLOYEE_NUM - 1) % count
+                                slide_offset = -width
+                                slide_direction = 1
+                                sliding = True
+                                debounce = False
+                           
                     #Checking if you pressed the right key
                     if event.key == pygame.K_RIGHT:
                         if not sliding:
-                            EMPLOYEE_NUM = (EMPLOYEE_NUM + 1) % count
-                            slide_offset = width
-                            slide_direction = -1
-                            sliding = True
-                            lines = get_employee_lines(EMPLOYEE_NUM)
-
-            #Animate slide
-            if sliding:
-                if slide_direction == -1:
-                    slide_offset -= SLIDE_SPEED
-                    if slide_offset <= 0:
-                        slide_offset = 0
-                        sliding = False
-
-                else:
-                    slide_offset += SLIDE_SPEED
-                    if slide_offset >= 0:
-                        slide_offset = 0
-                        sliding = False
-                    for i, (lbl, val, lc, vc) in enumerate(cards_left):
-                        y = start_y + i * (card_h + margin)
-                        draw_card(panel_x + margin, y, card_w, card_h, lbl, val, lc, vc)
-
-                    for i, (lbl, val, lc, vc) in enumerate(cards_right):
-                        y = start_y + i * (card_h + margin)
-                        draw_card(panel_x + margin*2 + card_w, y, card_w, card_h, lbl, val, lc, vc)
-
+                            if debounce == False:
+                                debounce = True
+                                EMPLOYEE_NUM = (EMPLOYEE_NUM + 1) % count
+                                slide_offset = width
+                                slide_direction = -1
+                                sliding = True
+                                debounce = False
+            
             #Setting the dark background
             screen.fill((10, 10, 20))
 
@@ -411,13 +353,6 @@ class Company:
                 ("Overtime Hours",lines['over'],  white, white),
             ]
 
-            card_left_2 = [
-                ("Name",          lines['name'],  white, gold),
-                ("Regular Hours Pay",  lines['reg_hours_pay'], white, white),
-                ("Holiday Hours Pay", lines['hol_hours_pay'],   white, white),
-                ("Over Hours Pay", lines['over_hours_pay'],   white, white),
-                ("401K Deduction",lines['_401k_deduct'],  white, white),
-            ]
             cards_right_1 = [
                 ("Hourly Rate",   lines['rate'],  white, gold),
                 ("401K Rate",     lines['401k'],  white, white),
@@ -425,21 +360,35 @@ class Company:
                 ("Gross Pay",     lines['gross'], white, gold),
                 ("Net Pay",       lines['net'],   white, green),
             ]
-            cards_right_2 = [
-                ("Roth Deduction",   lines['roth_deduct'],  white, white),
-                ("SS/Insurance Deduction",     lines['ss_tax_deduct'],  white, white),
-                ("State Deduction",     lines['state_tax_deduct'],  white, white),
-                ("Federal Deduction",     lines['fed_tax_deduct'], white, white),
-                ("Net Pay",  lines['net'],   white, green),
-            ]
 
-            for i, (lbl, val, lc, vc) in enumerate(cards_left):
+            for i, (lbl, val, lc, vc) in enumerate(cards_left_1):
                 y = start_y + i * (card_h + margin)
                 draw_card(panel_x + margin, y, card_w, card_h, lbl, val, lc, vc)
 
-            for i, (lbl, val, lc, vc) in enumerate(cards_right):
+            for i, (lbl, val, lc, vc) in enumerate(cards_right_1):
                 y = start_y + i * (card_h + margin)
                 draw_card(panel_x + margin*2 + card_w, y, card_w, card_h, lbl, val, lc, vc)
+
+                        #Animate slide
+            if sliding:
+                if slide_direction == -1:
+                    slide_offset -= SLIDE_SPEED
+                    if slide_offset <= 0:
+                        slide_offset = 0
+                        sliding = False
+
+                else:
+                    slide_offset += SLIDE_SPEED
+                    if slide_offset >= 0:
+                        slide_offset = 0
+                        sliding = False
+                    for i, (lbl, val, lc, vc) in enumerate(cards_left_1):
+                        y = start_y + i * (card_h + margin)
+                        draw_card(panel_x + margin, y, card_w, card_h, lbl, val, lc, vc)
+
+                    for i, (lbl, val, lc, vc) in enumerate(cards_right_1):
+                        y = start_y + i * (card_h + margin)
+                        draw_card(panel_x + margin*2 + card_w, y, card_w, card_h, lbl, val, lc, vc)
 
             #Draw arrow buttons
             left_hovered  = arrow_left_rect.collidepoint(mouse_pos)
@@ -448,7 +397,6 @@ class Company:
             if use_images:
                 screen.blit(img1, img_rect1)
                 screen.blit(img,  img_rect)
-                screen.blit(flipped, flipped_rect)
             else:
                 draw_arrow_button(arrow_left_rect,  'left',  left_hovered)
                 draw_arrow_button(arrow_right_rect, 'right', right_hovered)
